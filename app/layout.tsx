32afe605a344d61abd380a8a94a48313d5d80919
Christina Luna <christina.mendez@sbdinc.com>
2024-08-18 16:51:04 -0700

    Adds vercel speed insights.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
  
 export const metadata: Metadata = {
   title: {
--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,14 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <Analytics />
+      <SpeedInsights />
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
